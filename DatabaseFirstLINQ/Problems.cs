3c8c5ebbf5f08599c5c25087882bec154ad5ba2c
joeblk74 <joeblk@gmail.com>
2021-10-22 08:14:31 -0800

    Display of user shopping cart info

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -22,7 +22,7 @@ namespace DatabaseFirstLINQ
             //ProblemFive();
             //ProblemSix();
             //ProblemSeven();
-            //ProblemEight();
+            ProblemEight();
             //ProblemNine();
             //ProblemTen();
             //ProblemEleven();
@@ -150,6 +150,15 @@ namespace DatabaseFirstLINQ
 
         private void ProblemEight()
         {
+            var userProducts = _context.ShoppingCarts
+                                       .Include(ur => ur.User)
+                                       .Where(ue => ue.User.Email == "afton@gmail.com")
+                                       .Include(pn => pn.Product);
+            foreach (var product in userProducts)
+            {
+                Console.WriteLine($"product Name: {product.Product.Name} Price: {product.Product.Price} Quantity: {product.Quantity}");
+            }
+
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "afton@gmail.com".
             // Then print the product's name, price, and quantity to the console.
 
